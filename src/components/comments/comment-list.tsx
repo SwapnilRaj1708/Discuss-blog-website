f92baca7cc0f5375d0b6effc1f689cc8fad73020
Swapnil Raj <raj.swapnil1708@gmail.com>
2024-01-16 01:11:56 +0530

    refactor: Refactor comment fetching in CommentList and CommentShow

--- src/components/comments/comment-list.tsx
+++ src/components/comments/comment-list.tsx
@@ -1,23 +1,19 @@
 import CommentShow from "@/components/comments/comment-show";
-import { CommentWithAuthor } from "@/db/queries/comment";
+import { CommentWithAuthor, fetchCommentsByPostId } from "@/db/queries/comment";
 
 interface CommentListProps {
-  fetchData: () => Promise<CommentWithAuthor[]>;
+  postId: string;
 }
 
 // TODO: Get a list of comments from somewhere
-export default async function CommentList({ fetchData }: CommentListProps) {
-  const comments = await fetchData();
+export default async function CommentList({ postId }: CommentListProps) {
+  const comments = await fetchCommentsByPostId(postId);
   const topLevelComments = comments.filter(
     (comment) => comment.parentId === null
   );
   const renderedComments = topLevelComments.map((comment) => {
     return (
-      <CommentShow
-        key={comment.id}
-        commentId={comment.id}
-        comments={comments}
-      />
+      <CommentShow key={comment.id} commentId={comment.id} postId={postId} />
     );
   });
 
